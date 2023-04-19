f17811b4255876dce62a6cba2e8c2971ed5c4818
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-19 20:59:12 +0300

    Update Scroll.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export const Scroll=(props)=>{
+const Scroll=(props)=>{
     return (
         <div style={{overflowY: 'scroll', border:'5px solid black', height:'800px'}}>
             {props.children}
         </div>
     ); 
 };
+
+export default Scroll;
 
